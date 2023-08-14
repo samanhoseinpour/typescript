ea1b4243ab5c3983a9848cb713c5feb9e54fc48f
Saman <samangithoseinpour@gmail.com>
2023-08-14 10:03:33 +0330

    feat: implement User class

---
+++ maps/src/User.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.person.fullName();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,5 @@
-console.log('The ts file is running');
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
