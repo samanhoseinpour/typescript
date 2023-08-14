140ff5c08bf39ae02cc071f4a32962e394bd14b4
Saman <samangithoseinpour@gmail.com>
2023-08-14 12:23:55 +0330

    feat: implement Map class and marker functionality

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,33 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,10 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
+const customMap = new CustomMap('map');
 
-console.log(user, company);
+customMap.addMarker(user);
+customMap.addMarker(company);
