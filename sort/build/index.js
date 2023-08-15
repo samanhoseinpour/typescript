abcc766955fbe169d524ac1d8a5c0999274fcfa0
Saman <samangithoseinpour@gmail.com>
2023-08-15 13:55:57 +0330

    feat: sort array of number: number[]

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,14 +1,24 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Product = void 0;
-console.log('123');
-class Product {
-    constructor(price) {
-        this.price = price;
-        this.price = price;
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
     }
-    calcTotalPrice() {
-        console.log(this.price);
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                // if collection is an array of number: number[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+            }
+        }
     }
 }
-exports.Product = Product;
+const newArr = new Sorter([100, -3, -10, 1000, 50, 203]);
+newArr.sort();
+console.log(newArr);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,23 @@
-console.log('123');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-export class Product {
-  constructor(public price: number) {
-    this.price = price;
-  }
-
-  calcTotalPrice(): void {
-    console.log(this.price);
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // if collection is an array of number: number[]
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+      }
+    }
   }
 }
+
+const newArr = new Sorter([100, -3, -10, 1000, 50, 203]);
+newArr.sort();
+console.log(newArr);
