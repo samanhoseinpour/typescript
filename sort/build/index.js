c196cdfc2297c3bd1061c11727f66ffa83231cca
Saman <samangithoseinpour@gmail.com>
2023-08-15 18:51:00 +0330

    refactor: Sorter class

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,24 +1,6 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // if collection is an array of number: number[]
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-            }
-        }
-    }
-}
-const newArr = new Sorter([100, -3, -10, 1000, 50, 203]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const newArr = new Sorter_1.Sorter([100, -3, -10, 1000, 50, 203]);
 newArr.sort();
 console.log(newArr);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,22 +1,4 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // if collection is an array of number: number[]
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
-    }
-  }
-}
+import { Sorter } from './Sorter';
 
 const newArr = new Sorter([100, -3, -10, 1000, 50, 203]);
 newArr.sort();
