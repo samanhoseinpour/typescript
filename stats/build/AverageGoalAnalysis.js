5e763a099dc0b4b11afda170aa32a3bcbb6996ec
Saman <samangithoseinpour@gmail.com>
2023-08-31 12:31:46 +0330

    fix: remove extra files

--- stats/build/AverageGoalAnalysis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AverageGoalAnalysis = void 0;
-class AverageGoalAnalysis {
-}
-exports.AverageGoalAnalysis = AverageGoalAnalysis;
--- stats/build/reports/HtmlReport.js
+++ stats/build/reports/HtmlReport.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HtmlReport = void 0;
 class HtmlReport {
+    print(report) { }
 }
 exports.HtmlReport = HtmlReport;
