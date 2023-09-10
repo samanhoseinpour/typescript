498ce5388ececd670eee7d3471decfac3ea70a5f
Saman <samangithoseinpour@gmail.com>
2023-09-10 12:39:24 +0330

    feat: render data

--- redux/src/components/RepositoriesList.tsx
+++ redux/src/components/RepositoriesList.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-
 import { useActions } from '../hooks/useActions';
+
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
   const [searchRepository, setSearchRepository] = useState('');
   const { searchRepositories } = useActions();
+  const { data, loading, error } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const submitSearchRepository = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -21,6 +25,12 @@ const RepositoriesList: React.FC = () => {
         />
         <button>search</button>
       </form>
+      <ul>
+        {data &&
+          data.map((packages: string) => <li key={packages}>{packages}</li>)}
+        {loading && <h3>Loading...</h3>}
+        {error && <h3>{error}</h3>}
+      </ul>
     </div>
   );
 };
