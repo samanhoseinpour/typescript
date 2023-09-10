5b64cdaddfc0833a4785e8190df9cae00db017df
Saman <samangithoseinpour@gmail.com>
2023-09-10 10:25:56 +0330

    feat: implement Action Creator

---
+++ redux/src/store/action-creators/repositoriesActionCreator.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+
+import { ActionType } from '../action-types/repositoriesActionType';
+import { Action } from '../actions/repositoriesAction';
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
