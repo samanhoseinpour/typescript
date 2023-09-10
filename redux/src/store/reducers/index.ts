49b6549eeeccacaa7b066d619ff48f9218c5ea7d
Saman <samangithoseinpour@gmail.com>
2023-09-10 10:48:01 +0330

    feat: export redux stuff

---
+++ redux/src/store/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import repositoriesReducer from './repositoriesReducer';
+
+const reducers = combineReducers({
+  repositories: repositoriesReducer,
+});
+
+export default reducers;
